cc196a56c10d3c4d55df83a6cafc2ac1c08bf93c
SorestGaming14 <leeyura669@gmail.com>
2023-01-19 11:52:10 +0500

    complete

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var x = 'X'
+var y = 'Y'
+var z = 'Z'
+var res = res
+
+x = prompt('Добавьте значение для X:')
+console.log(x);
+
+y = prompt('Добавьте значение для Y:')
+console.log(y);
+
+z = prompt('Добавьте значение для Z:')
+console.log(z);
+
+var result = +x + +y + +z
+console.log(result);
+
+res = result / 3
+alert('Результат: ' + res);
+console.log(res);
